9a7e6187b8f31c09d0c6b36e5b846e2cd6e17676
Yasir Ali <sdr.yasir@gmail.com>
2024-02-04 02:54:40 -0800

    js

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+function add(a, b){
+  console.log(a+b)
+}
+function subtract(a, b){
+  console.log(a-b)
+}
+function multiply(a, b){
+  console.log(a*b)
+}
+function divide(a, b){
+  console.log(a/b)
+}
+
